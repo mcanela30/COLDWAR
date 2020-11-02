b35a1e89810d55aac65f525fccec10d0f9007575
gabrigomezayuso <ggomezayuso@gmail.com>
2020-11-02 17:04:49 +0100

    ahora si

---
+++ src/main.java
@@ -0,0 +1,101 @@
+//Gabriel Gomez Ayuso, Marti Canela
+//Coldwar - MAIN 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Date;
+
+public class main {
+	static Scanner sc = new Scanner(System.in);
+	static ArrayList<planeta> equipo = new ArrayList<planeta>();
+	static int ronda=1,N=0, equiposVivos=0,k=0;
+	public static int [] vidaAtaque;
+	static Date fecha = new Date();
+
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+		menu();//llamamos al menu
+	}
+
+	//esta funcion sirve para prinatar el menu
+	public static void menu() {
+
+		boolean salir = false;
+		int opcion = 5; //la inicializamos a 5 porque de esta forma si pone un caracter que no sea entre el 0 o el 4 le salga otra vex el menu
+		while(!salir){
+
+			System.out.println("1. JUGAR");
+			System.out.println("2. REGLAS DEL JUEGO");
+			System.out.println("3. INFORMACION");
+			System.out.println("4. APARTADO ABIERTO");
+			System.out.println("0. SALIR");
+			System.out.print("Escribe una de las opciones: ");
+
+			try {
+				opcion=sc.nextInt();
+			}catch(InputMismatchException ime) {// en caso que introducza una letra le va ha salir este error gracias a este catch.
+				System.out.println("ERROR. Està introduciendo una letra en vez de un numero. \n");
+				sc.next();
+			}
+			
+			partida partida = new partida();//llamamos a la clase partida.
+			
+			switch(opcion){//menu
+			case 1:
+				partida.iniciarPartida();//llamamos a la funcion inciarPartida() de la clase Partida.
+				if(N==1) {// en caso que sea igual que la variable N sea igual que 1 llamaremos a la siguiente funcion.
+					partida.finalizarPartida();
+					break;
+				}
+				break;
+			case 2:
+				System.out.println("\n------------------------------");
+				reglas();//llamamos a la funcion reglas
+				System.out.println("------------------------------\n");
+				break;
+			case 3:
+				System.out.println("\n------------------------------");
+				informacion();//llamamos a la funcion informacion
+				System.out.println("------------------------------\n");
+				break;
+			case 4:
+				System.out.println("\n------------------------------");
+				partida.apartadoAbierto();
+				System.out.println("------------------------------\n");
+				System.out.println(" ");
+				break;
+			case 0:
+				System.out.println("\nÃ¥ï¿½ï¿½-Ã¥ï¿½ï¿½-Ã¥ï¿½ï¿½-Ã¥ï¿½ï¿½-Ã¥ï¿½ï¿½-Ã¥ï¿½ï¿½-");
+				System.out.println("A la puta calle."); // salimos del juego
+				System.out.println("Ã¥ï¿½ï¿½-Ã¥ï¿½ï¿½-Ã¥ï¿½ï¿½-Ã¥ï¿½ï¿½-Ã¥ï¿½ï¿½-Ã¥ï¿½ï¿½-Ã¥");
+				salir=true; // ponemos el boolean a true para que no vuleva a entrar al while y no printe el menu. 
+				break;
+			default:
+				System.out.println("\nSolo numeros entre 0 y 4");// en caso que el usuario ponga otro numero que no sea del 0 al 4 entra aqui. 
+			}
+		}
+	}
+
+	//Esta funcion printa las reglas del juego.
+	public static void reglas() {
+		System.out.println("REGLAS DEL JUEGO:");
+		System.out.println("1. Cada equipo tiene 50 misiles por ronda. ");
+		System.out.println("2. Los misiles puden servirse de tanto de defensa como para atacar.");
+		System.out.println("3. Los misiles de defensa valen el doble, es decir 25 misiles\n de defensa equivalen a 50 misiles de ataque.");
+		System.out.println("4. Ã‚Â¡Mucha SUERTE!");
+	}
+
+	//Funciona para printar la informacion del juego.
+	public static void informacion() {
+		System.out.println("Informacion sobre el programa:");
+		System.out.println("- Version : 1.0");
+		System.out.println("Contacto: manda@unaayuda.com");
+		System.out.println("Autores: Gabriel Gomez y Marti Canela");
+	}
+
+}
+
+
+
+
+
---
+++ src/planeta.java
@@ -0,0 +1,80 @@
+//Marti Canela, Gabriel Gómez
+public class planeta {
+	
+	//Creamos las variables
+	private int vidas=200;
+	private String nombre=null;
+	private int misiles_ronda=0;
+	private int numero_equipos=0;
+	private boolean ataquedefansa=true;
+	private int misilesDefensa=0;
+	
+	
+	//Creamos Getters y Setters para poder coger las variables, al ser private, en los otros ficheros.
+	public int getMisilesDefensa() {
+		return misilesDefensa;
+	}
+
+	public void setMisilesDefensa(int misilesDefensa) {
+		this.misilesDefensa = misilesDefensa;
+	}
+
+	public planeta(String nombre, int numero_equipos) {
+		this.nombre = nombre;
+		this.vidas = 25;
+		this.misiles_ronda = 50;
+		this.numero_equipos = numero_equipos;
+	}
+	
+	public int getVidas() {
+		return vidas;
+	}
+	public void setVidas(int vida, int vidas) {
+		this.vidas = vidas-vida;
+	}
+	public void setVidasDefensa(int defensa) {
+		if (ataquedefansa==false) {
+		this.vidas = vidas+defensa;
+		}		
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getMisiles_ronda() {
+		return misiles_ronda;
+	}
+	public void setMisiles_ronda(int misiles_ronda) {
+		this.misiles_ronda = misiles_ronda;
+	}
+	public int getNumero_equipos() {
+		return numero_equipos;
+	}
+	public void setNumero_equipos(int numero_equipos) {
+		this.numero_equipos = numero_equipos;
+	}
+	
+	public void quitarVida() {
+		vidas=vidas+misilesDefensa;
+	}
+	
+	public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Equipo "+numero_equipos+": "+nombre+" --> "+vidas); 
+        return sb.toString();
+    }
+	
+	
+	
+	
+
+
+
+
+		
+	}
+
+
+
